96d60854ecf31d3fe2a3efeb97e0a3a531e661c0
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-05-22 08:00:06 +0530

    feat(auth): add login and register routes

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,14 @@
 import {createBrowserRouter} from 'react-router'
-
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 
 export const router = createBrowserRouter([
-  
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  }
 ])
