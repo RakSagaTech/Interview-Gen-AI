f4cc65178f70fbb6ceff6528140d45421599ab14
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-05-23 17:36:16 +0530

    feat(auth): add login form handler and shared auth styles

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { Link } from 'react-router'
+import '../auth.form.scss'
 
 const Login = () => {
+
+
+  const handleLogin = (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required placeholder="Enter your email" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" required placeholder="Enter your username" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required placeholder="Enter your password" />
+          </div>
+          <button className="button primary-button">Login</button>
+        </form>
+        <p>Don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </main>
   )
 }
 
