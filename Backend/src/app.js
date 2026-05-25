97f40ad16974ad5457793836a7cfe94b4e5e5d89
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-05-25 22:56:11 +0530

    feat(server): install and configure CORS middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser')
 
 /*Require all the routes here*/
@@ -7,6 +8,10 @@ const authRouter = require('./routes/auth.routes');
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 app.use(cookieParser());
 
 
