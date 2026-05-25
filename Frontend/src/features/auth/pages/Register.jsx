5d5d362214d7d8a8c33784147dd8cce260dd0277
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-05-25 23:08:45 +0530

    feat(auth): implement register form state and submission handling

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,28 +1,42 @@
-import { Link } from 'react-router'
+import { Link} from 'react-router'
+import {useState} from 'react'
+import {useAuth} from '../hooks/useAuth'
 import '../auth.form.scss'
 
 const Register = () => {
 
-  const handleRegister = (e) =>{
+  const {loading, handleRegister} = useAuth()
+
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleSubmit = async(e) =>{
     e.preventDefault()
+    await handleRegister({email, username, password})
+  }
+
+  if(loading){
+    return (<main><h1>Loading...</h1></main>)
   }
 
   return (
     <main>
       <div className="form-container">
         <h1>Register</h1>
-        <form onSubmit={handleRegister}>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" required placeholder="Enter your email" />
+            <input type="email" id="email" name="email" required placeholder="Enter your email" onChange={(e) => {setEmail(e.target.value)}}/>
           </div>
           <div className="input-group">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" required placeholder="Enter your username" />
+            <input type="text" id="username" name="username" required placeholder="Enter your username" onChange={(e) => {setUsername(e.target.value)}}/>
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required placeholder="Enter your password" />
+            <input type="password" id="password" name="password" required placeholder="Enter your password" onChange={(e) => {setPassword(e.target.value)}}/>
           </div>
           <button className="button primary-button">Register</button>
         </form>
