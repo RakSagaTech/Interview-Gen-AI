507e08154aa86a77563fb445f721d0850a27a217
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-05-25 21:24:12 +0530

    feat: create AuthContext and AuthProvider, wrap App with AuthProvider

---
+++ Frontend/src/features/auth/context/auth.provider.jsx
@@ -0,0 +1,14 @@
+import {useContext, useState} from "react";
+import {AuthContext} from "./auth.context.jsx" 
+
+
+export const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(false) 
+
+  return (
+    <AuthContext.Provider value={{user, setUser, loading, setLoading}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
